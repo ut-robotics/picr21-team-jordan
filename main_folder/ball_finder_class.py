5a4cc08859a3ee3589a46f51816884a519ae41a1
our robot <blabla@blala.com>
2021-09-19 15:54:04 +0300

    added biggest ball detection by blob

--- main_folder/ball_finder_class.py
+++ main_folder/ball_finder_class.py
@@ -18,7 +18,7 @@ class BallFinder:
         self.fps: int = 0
 
         self.color_type = color_type
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture(4)
         # self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         # self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
@@ -39,7 +39,7 @@ class BallFinder:
 
         self.blobparams = cv2.SimpleBlobDetector_Params()
         self.blobparams.filterByArea = True
-        self.blobparams.minArea = 1000
+        self.blobparams.minArea = 300
         self.blobparams.maxArea = 999999
         self.blobparams.filterByInertia = False
         self.blobparams.filterByConvexity = False
@@ -57,6 +57,7 @@ class BallFinder:
         white ball and black background
         use betwise_not(mask) if needed
         """
+        inspected_frame = cv2.bitwise_not(inspected_frame)
         cnts = cv2.findContours(inspected_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         center = None
@@ -88,13 +89,24 @@ class BallFinder:
         black ball and whiite background
         use betwise_not(mask) if needed
         """
+        # for debbuging and finding blobs
+        contours, _ = cv2.findContours(inspected_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            approx = cv2.approxPolyDP(cnt, 0.001 * cv2.arcLength(cnt, True), True)
+            cv2.drawContours(target_frame, [approx], 0, (255, 0, 0), 2)
+        
+        
         keypoints: list = self.detector.detect(inspected_frame)
         cv2.drawKeypoints(
             inspected_frame, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
         )
+
+        #detect all keypoints
+        kp_sizes = []
         if len(keypoints) > 0:
             for keypoint in keypoints:
-                if keypoint.size > 60:
+                kp_sizes.append(keypoint.size)
+                if keypoint.size > 30:
                     text = (
                         str(round(keypoint.pt[0]))
                         + " : "
@@ -105,18 +117,22 @@ class BallFinder:
                     x, y = int(keypoint.pt[0]), int(keypoint.pt[1])
                     cv2.putText(target_frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                    """
-                    If there's more than one ball, programm can't define which keypoint is actually a which ball,
-                    because list of keypoints is recreated after each iteration.
-                    To avoid that mess, programm will remember last coordinates of the ball and move to the new state,
-                    in the new state robot moves to the ball to grab him.
-                    """
-
-        # foor debbuging and finding blobs
-        contours, _ = cv2.findContours(inspected_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            approx = cv2.approxPolyDP(cnt, 0.001 * cv2.arcLength(cnt, True), True)
-            cv2.drawContours(target_frame, [approx], 0, (0, 0, 255), 2)
+        #detects biggest keypoint, marks with different color
+        try:
+            biggest_keypoint_index = kp_sizes.index(max(kp_sizes))
+            biggest_keypoint = keypoints[biggest_keypoint_index]
+            x, y = int(biggest_keypoint.pt[0]), int(biggest_keypoint.pt[1])
+            text = (
+                        str(round(biggest_keypoint.pt[0]))
+                        + " : "
+                        + str(round(biggest_keypoint.pt[1]))
+                        + ":::"
+                        + str(round(biggest_keypoint.size))
+                    )
+            cv2.putText(target_frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        except ValueError:
+            pass
+        
 
     def main(self):
         while True:
@@ -144,7 +160,7 @@ class BallFinder:
             mask = cv2.inRange(hsv_blured, lowerLimits, upperLimits)
             mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel1)
             mask = cv2.dilate(mask, kernel2, iterations=1)
-            # mask = cv2.bitwise_not(mask)
+            mask = cv2.bitwise_not(mask)
 
             # self.track_ball_using_imutils(mask, frame)
             self.track_ball_using_blob(mask, frame)
@@ -155,7 +171,9 @@ class BallFinder:
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 input_manager.save_default_values(
-                    self.path, "trackbar_values", [str(x) for x in [hl, sl, vl, hh, sh, vh, dil1, dil2, clos1, clos2]]
+                    self.path,
+                    "trackbar_values_ball",
+                    [str(x) for x in [hl, sl, vl, hh, sh, vh, dil1, dil2, clos1, clos2]],
                 )
                 break
 
