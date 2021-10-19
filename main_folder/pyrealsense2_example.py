cb31f5c6f564ce76ec44966f635550c9011fecc4
LeonidTsigrinski <Leonid.tsigrinski@gmail.com>
2021-10-19 18:12:20 +0300

    pyreralsensee eexample

--- main_folder/pyrealsense2_example.py
+++ main_folder/pyrealsense2_example.py
@@ -10,7 +10,6 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start(config)
-fps_array = np.array([])
 
 try:
     while True:
