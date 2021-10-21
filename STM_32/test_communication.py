3422166d8152b144988fafa2724af064cacfdfe3
Valentin Resapow <valentin.resapow@tuhh.de>
2021-10-21 15:07:02 +0300

    added direction control

--- STM_32/test_communication.py
+++ STM_32/test_communication.py
@@ -1,6 +1,7 @@
 import struct
 import serial
 import time
+import math
 
 
 ser = serial.Serial("/dev/ttyACM0", 115200)
@@ -10,25 +11,36 @@ if not ser.isOpen():
 
 print(ser.isOpen())
 
+goal_dir_angle = -90
+speed_limit = 30
 
 disable_failsafe = 0
-speed1 = 10
-speed2 = 10
-speed3 = 10
-thrower_speed = 10
+speed1 = int(math.sin((-goal_dir_angle+60)*(2*math.pi/360))*(speed_limit))
+speed2 = int(math.sin((-goal_dir_angle+180)*(2*math.pi/360))*(speed_limit))
+speed3 = int(math.sin((-goal_dir_angle+240)*(2*math.pi/360))*(speed_limit))
+
+# print(math.sin(goal_dir_angle+60*(2*math.pi/360)))
+print(speed1)
+print(speed2)
+print(speed3)
+
+# speed1 = 0
+# speed2 = 0
+# speed3 = 0
+thrower_speed = 0
 
 try:
     while True:
-        send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, disable_failsafe, 0xAAAA)
+        send_data = struct.pack("<hhhHBH", speed1, speed2,
+                                speed3, thrower_speed, disable_failsafe, 0xAAAA)
         ser.write(send_data)
-        time.sleep(0.5)
         received_data = ser.read(8)
-        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
+        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack(
+            "<hhhH", received_data)
         print(actual_speed1, "actual_speed1")
         print(actual_speed2, "actual_speed2")
         print(actual_speed3, "actual_speed3")
         print(feedback_delimiter, "feedback_delimiter")
-        time.sleep(0.5)
-        print("hello")
+        # print("hello")
 except KeyboardInterrupt:
     ser.close()
