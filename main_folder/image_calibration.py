00bbb22117404d3151d490d35c2455a5f5281a4f
our robot <blabla@blala.com>
2021-10-21 14:42:31 +0300

    reached 60 fps

--- main_folder/image_calibration.py
+++ main_folder/image_calibration.py
@@ -17,8 +17,12 @@ class ImageCalibraion:
         if enable_pyrealsense:
             self.pipeline = rs.pipeline()
             config = rs.config()
-            config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+            
+            # config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+            # config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+            config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
+            config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 60)
+            
             self.pipeline.start(config)
             self.ALPHA_DEPTH = 0.9
         else:
