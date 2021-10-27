64fcafa5074a5de955ef980138127687d3b9239e
our robot <blabla@blala.com>
2021-10-27 14:14:42 +0300

    minor bug fix

--- STM_32/move_function.py
+++ STM_32/move_function.py
@@ -6,7 +6,7 @@ import math
 
 def move(serial_port="ttyACM0", moving_direction=0, speed_limit=10, thrower_speed=0, failsafe=0):
 
-    dir_serial = "/dev/"+serial_port
+    dir_serial = "/dev/" + serial_port
 
     ser = serial.Serial(dir_serial, 115200)
     if not ser.isOpen():
@@ -19,19 +19,16 @@ def move(serial_port="ttyACM0", moving_direction=0, speed_limit=10, thrower_spee
     # thrower_speed = 1200
     # failsafe = 0
 
-    speed1 = int(math.sin((moving_direction+120)
-                 * (2*math.pi/360))*(speed_limit))
-    speed2 = int(math.sin((moving_direction)*(2*math.pi/360))*(speed_limit))
-    speed3 = int(math.sin((moving_direction-120)*(2*math.pi/360))*(speed_limit))
+    speed1 = int(math.sin((moving_direction + 120) * (2 * math.pi / 360)) * (speed_limit))
+    speed2 = int(math.sin((moving_direction) * (2 * math.pi / 360)) * (speed_limit))
+    speed3 = int(math.sin((moving_direction - 120) * (2 * math.pi / 360)) * (speed_limit))
 
     try:
         while True:
-            send_data = struct.pack("<hhhHBH", speed1, speed2,
-                                    speed3, thrower_speed, failsafe, 0xAAAA)
+            send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
             ser.write(send_data)
             received_data = ser.read(8)
-            actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack(
-                "<hhhH", received_data)
+            actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
             # print(actual_speed1, "actual_speed1")
             # print(actual_speed2, "actual_speed2")
             # print(actual_speed3, "actual_speed3")
