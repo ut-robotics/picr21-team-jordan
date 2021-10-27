e7e6192d60a5f21b36b1e9b4cd98c3a0ce599eed
Valentin Resapow <valentin.resapow@tuhh.de>
2021-10-27 14:17:14 +0300

    remove merge conflict

--- STM_32/move_function.py
+++ STM_32/move_function.py
@@ -4,9 +4,8 @@ import time
 import math
 
 
-<<<<<<< HEAD
 def move_robot(serial_port="ttyACM0", moving_direction=0, speed_lime=10, thrower_speed=0, failsafe=0):
-    """Function for moving the robot and controling of the thrower. 
+    """Function for moving the robot and controling of the thrower.
         The function will use serial communication to communicate with with STM32.
 
     Args:
@@ -16,14 +15,10 @@ def move_robot(serial_port="ttyACM0", moving_direction=0, speed_lime=10, thrower
         thrower_speed (int, optional): PWM value for controlling the thrower (from 0 to ca. 2036). Defaults to 0.
         failsafe (int, optional): booline value to indicate in the robot should repeat the last command. Defaults to 0.
     """
-=======
-def move(serial_port="ttyACM0", moving_direction=0, speed_limit=10, thrower_speed=0, failsafe=0):
->>>>>>> 43d76af8e45b5e075dc91535692f9b9f5ca62a42
+ dir_serial = "/dev/"+serial_port
 
-    dir_serial = "/dev/"+serial_port
-
-    ser = serial.Serial(dir_serial, 115200)
-    if not ser.isOpen():
+  ser = serial.Serial(dir_serial, 115200)
+   if not ser.isOpen():
         ser.open()
 
     print(ser.isOpen())
@@ -31,7 +26,8 @@ def move(serial_port="ttyACM0", moving_direction=0, speed_limit=10, thrower_spee
     speed1 = int(math.sin((moving_direction+120)
                  * (2*math.pi/360))*(speed_limit))
     speed2 = int(math.sin((moving_direction)*(2*math.pi/360))*(speed_limit))
-    speed3 = int(math.sin((moving_direction-120)*(2*math.pi/360))*(speed_limit))
+    speed3 = int(math.sin((moving_direction-120)
+                 * (2*math.pi/360))*(speed_limit))
 
     try:
         while True:
