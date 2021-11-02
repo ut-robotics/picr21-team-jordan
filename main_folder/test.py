9c5d97e00fe527b5110b865500421555665f3b46
Leonid-98 <leonid.tsigrinski@gmail.com>
2021-11-02 21:17:34 +0200

    minbor

--- main_folder/test.py
+++
@@ -1,6 +0,0 @@
-p = {"USB VID:PID=0483:5740 SER=207738905056 LOCATION=1-3:1.0": "ttyACM0", "1":"2","3":"2", "4":"2"}
-for key in p.keys():
-    print(key, 228)
-    if "USB VID:PID=0483:5740" in key:
-        print(p[key])
-        break
