f41a354a10f49cce6817d548ecedcfd73fa4699c
Valentin Resapow <valentin.resapow@tuhh.de>
2021-11-04 18:46:45 +0200

    changed some errors

--- main_folder/move_function2.py
+++ main_folder/move_function2.py
@@ -6,35 +6,44 @@ import serial
 DEFAULT_SERIAL_PORT = "/dev/ttyACM0"
 
 # Inputs
-robotSpeedX = 0 # - m/s
-robotSpeedY = 0 # - m/s
+robotSpeedX = 0  # - m/s
+robotSpeedY = 0  # - m/s
 
 # motor variables
 gearboxReductionRatio = 18.75
 encoderEdgesPerMotorRevolution = 64
 wheelRadius = 0.035  # $[m]
-pidControlFrequency = 60 # [Hz]
-wheelLinearVelocity = 2 # [m/s]
+pidControlFrequency = 60  # [Hz]
+wheelLinearVelocity = 2  # [m/s]
 
 
-def cal_robotSpeed(robotSpeedX, robotSpeedX):
-    robotSpeed = math.sqrt(robotSpeedX * robotSpeedX + robotSpeedY * robotSpeedY)
+def cal_robotSpeed(robotSpeedX, robotSpeedY):
+    robotSpeed = math.sqrt(robotSpeedX * robotSpeedX +
+                           robotSpeedY * robotSpeedY)
     return robotSpeed
 
-def cal_robotDirectionAngle(robotSpeedX, robotSpeedX):
+
+def cal_robotDirectionAngle(robotSpeedX, robotSpeedY):
     robotDirectionAngle = math.atan2(robotSpeedY, robotSpeedX)
     return robotDirectionAngle
 
-def cal_speed(robotDirectionAngle, wheelAngle, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity):
-    wheelLinearVelocity = robotSpeed * cos(robotDirectionAngle - wheelAngle) + wheelDistanceFromCenter * robotAngularVelocity 
+
+def cal_speed(robotSpeed, robotDirectionAngle, wheelAngle, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity):
+    wheelLinearVelocity = robotSpeed * \
+        math.cos(robotDirectionAngle - wheelAngle) + \
+        wheelDistanceFromCenter * robotAngularVelocity
     return wheelLinearVelocity
 
+
 def cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency):
-    wheelSpeedToMainboardUnits = gearboxReductionRatio * encoderEdgesPerMotorRevolution / (2 * PI * wheelRadius * pidControlFrequency) 
-    wheelAngularSpeedMainboardUnits = wheelLinearVelocity * wheelSpeedToMainboardUnits 
+    wheelSpeedToMainboardUnits = gearboxReductionRatio * \
+        encoderEdgesPerMotorRevolution / \
+        (2 * math.pi * wheelRadius * pidControlFrequency)
+    wheelAngularSpeedMainboardUnits = wheelLinearVelocity * wheelSpeedToMainboardUnits
     return wheelAngularSpeedMainboardUnits
 
-def move_robot(state="translation", robotSpeedX, robotSpeedY, speed_limit=0.5, thrower_speed=0, failsafe=0):
+
+def move_robot(robotSpeedX, robotSpeedY, speed_limit=0.5, thrower_speed=0, failsafe=0, state="translation"):
 
     ser = serial.Serial(DEFAULT_SERIAL_PORT, 115200)
     if not ser.isOpen():
@@ -43,15 +52,21 @@ def move_robot(state="translation", robotSpeedX, robotSpeedY, speed_limit=0.5, t
     if state == "translation":
         robotSpeed = cal_robotSpeed(robotSpeedX, robotSpeedY) * speed_limit
         robotDirection = cal_robotDirectionAngle(robotSpeedX, robotSpeedY)
-     
-        wheelLinearVelocity_1 = cal_speed(robotDirectionAngle, wheelAngle_1, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed1 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
-          
-        wheelLinearVelocity_2 = cal_speed(robotDirectionAngle, wheelAngle_2, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed2 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
-           
-        wheelLinearVelocity_3 = cal_speed(robotDirectionAngle, wheelAngle_3, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed3 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+
+        wheelLinearVelocity_1 = cal_speed(
+            robotDirectionAngle, wheelAngle_1, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed1 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
+                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+
+        wheelLinearVelocity_2 = cal_speed(
+            robotDirectionAngle, wheelAngle_2, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed2 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
+                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+
+        wheelLinearVelocity_3 = cal_speed(
+            robotDirectionAngle, wheelAngle_3, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed3 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
+                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
 
         """
         !TODO delete this after tested on robot
@@ -68,16 +83,19 @@ def move_robot(state="translation", robotSpeedX, robotSpeedY, speed_limit=0.5, t
         raise Exception("Invalid state arrgument.")
 
     try:
-        send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
+        send_data = struct.pack("<hhhHBH", speed1, speed2,
+                                speed3, thrower_speed, failsafe, 0xAAAA)
         ser.write(send_data)
         received_data = ser.read(8)
-        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
+        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack(
+            "<hhhH", received_data)
         # for debugging
-        print(f"{speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}{actual_speed3}")
+        print(
+            f"{speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}{actual_speed3}")
         # print(feedback_delimiter, "feedback_delimiter")
     except KeyboardInterrupt:
         pass
 
 
 if __name__ == "__main__":
-    move_robot("ttyACM0", moving_direction=0, speed_limit=0)
+    move_robot(robotSpeedX=2, robotSpeedY=0, speed_limit=0.5)
