6373fa537ce5ca3ab8be742ec8697b4b9c99153c
Leonid-98 <leonid.tsigrinski@gmail.com>
2021-11-08 17:00:43 +0200

    minor fix

--- main_folder/move_function2.py
+++ main_folder/move_function2.py
@@ -18,8 +18,7 @@ wheelLinearVelocity = 2  # [m/s]
 
 
 def cal_robotSpeed(robotSpeedX, robotSpeedY):
-    robotSpeed = math.sqrt(robotSpeedX * robotSpeedX +
-                           robotSpeedY * robotSpeedY)
+    robotSpeed = math.sqrt(robotSpeedX * robotSpeedX + robotSpeedY * robotSpeedY)
     return robotSpeed
 
 
@@ -29,16 +28,12 @@ def cal_robotDirectionAngle(robotSpeedX, robotSpeedY):
 
 
 def cal_speed(robotSpeed, robotDirectionAngle, wheelAngle, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity):
-    wheelLinearVelocity = robotSpeed * \
-        math.cos(robotDirectionAngle - wheelAngle) + \
-        wheelDistanceFromCenter * robotAngularVelocity
+    wheelLinearVelocity = robotSpeed * math.cos(robotDirectionAngle - wheelAngle) + wheelDistanceFromCenter * robotAngularVelocity
     return wheelLinearVelocity
 
 
 def cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency):
-    wheelSpeedToMainboardUnits = gearboxReductionRatio * \
-        encoderEdgesPerMotorRevolution / \
-        (2 * math.pi * wheelRadius * pidControlFrequency)
+    wheelSpeedToMainboardUnits = gearboxReductionRatio * encoderEdgesPerMotorRevolution / (2 * math.pi * wheelRadius * pidControlFrequency)
     wheelAngularSpeedMainboardUnits = wheelLinearVelocity * wheelSpeedToMainboardUnits
     return wheelAngularSpeedMainboardUnits
 
@@ -53,20 +48,14 @@ def move_robot(robotSpeedX, robotSpeedY, speed_limit=0.5, thrower_speed=0, fails
         robotSpeed = cal_robotSpeed(robotSpeedX, robotSpeedY) * speed_limit
         robotDirection = cal_robotDirectionAngle(robotSpeedX, robotSpeedY)
 
-        wheelLinearVelocity_1 = cal_speed(
-            robotDirectionAngle, wheelAngle_1, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed1 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
-                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+        wheelLinearVelocity_1 = cal_speed(robotDirectionAngle, wheelAngle_1, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed1 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
 
-        wheelLinearVelocity_2 = cal_speed(
-            robotDirectionAngle, wheelAngle_2, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed2 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
-                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+        wheelLinearVelocity_2 = cal_speed(robotDirectionAngle, wheelAngle_2, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed2 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
 
-        wheelLinearVelocity_3 = cal_speed(
-            robotDirectionAngle, wheelAngle_3, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
-        speed3 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio,
-                                   encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
+        wheelLinearVelocity_3 = cal_speed(robotDirectionAngle, wheelAngle_3, wheelDistanceFromCenter, robotAngularVelocity, wheelLinearVelocity)
+        speed3 = cal_seed_to_board(wheelLinearVelocity, gearboxReductionRatio, encoderEdgesPerMotorRevolution, wheelRadius, pidControlFrequency)
 
         """
         !TODO delete this after tested on robot
@@ -76,22 +65,19 @@ def move_robot(robotSpeedX, robotSpeedY, speed_limit=0.5, thrower_speed=0, fails
         """
 
     elif state == "rotation":
-        speed1 = speed_limit*120
-        speed2 = speed_limit*120
-        speed3 = speed_limit*120
+        speed1 = speed_limit * 120
+        speed2 = speed_limit * 120
+        speed3 = speed_limit * 120
     else:
         raise Exception("Invalid state arrgument.")
 
     try:
-        send_data = struct.pack("<hhhHBH", speed1, speed2,
-                                speed3, thrower_speed, failsafe, 0xAAAA)
+        send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
         ser.write(send_data)
         received_data = ser.read(8)
-        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack(
-            "<hhhH", received_data)
+        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
         # for debugging
-        print(
-            f"{speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}{actual_speed3}")
+        print(f"{speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}{actual_speed3}")
         # print(feedback_delimiter, "feedback_delimiter")
     except KeyboardInterrupt:
         pass
