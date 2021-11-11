635ba23792c368696c40795e116dcb3c44bb1b40
Leonid-98 <root@root.com>
2021-11-11 18:52:44 +0200

    reafctored robot movent

--- main_folder/robot_movement.py
+++ main_folder/robot_movement.py
@@ -12,7 +12,7 @@ class SerialPortNotFound(Exception):
 
 
 STM_32_HWID = "USB VID:PID=0483:5740"
-WHEEL_ANGLES = [+120, 0, -120]
+WHEEL_ANGLES = [-120, 0, 120]
 
 
 class RobotMovement:
@@ -34,39 +34,25 @@ class RobotMovement:
         self.ser = serial.Serial(serial_port, 115200)
 
     def calculate_speed(self, wheel_angle_degrees, moving_direction, speed, rotation_speed):
-        return int(math.sin((moving_direction + wheel_angle_degrees) * (2 * math.pi / 360)) * speed + rotation_speed)
+        return int(round(math.cos((moving_direction + wheel_angle_degrees) * (2 * math.pi / 360)) * speed + rotation_speed))
 
     def move_robot(self, moving_direction=0, speed=0, rotation_speed=0, thrower_speed=0, failsafe=0):
         speed1, speed2, speed3 = [self.calculate_speed(angle, moving_direction, speed, rotation_speed) for angle in WHEEL_ANGLES]
-
         send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
-        
-        
         self.ser.write(send_data)
 
-    def move_robot_XY(self, speed_x=0, speed_y=0, rotation_speed=0, thrower_speed=0):
-        def degrees(rad):
-            return(rad * 180 / math.pi )
+        received_data = self.ser.read(8)
+        actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
+        print(f"calucalted/actual | {speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}/{actual_speed3}")
 
-        pure_deg = degrees(math.atan2(speed_y, speed_x))
-        moving_direction = 0
-        if speed_y == 0:
-            moving_direction = 90 if speed_x > 0 else 270
-        elif speed_x == 0:
-            moving_direction = 180 if speed_x > 0 else 0
-        elif speed_x > 0 and speed_y > 0:
-            moving_direction = 90 + pure_deg 
-        elif speed_x > 0 and speed_y < 0: 
-            moving_direction = 90 - pure_deg
-        elif speed_x < 0 and speed_y < 0:
-            moving_direction = 270 + pure_deg
-        elif speed_x < 0 and speed_y > 0:
-            moving_direction = 180 + pure_deg
-        print(moving_direction)    
+
+    def move_robot_XY(self, speed_x=0, speed_y=0, rotation_speed=0, thrower_speed=0):
+        moving_direction = (math.atan2(speed_y, speed_x)) * 180 / math.pi
         speed = math.hypot(speed_x, speed_y)  # vector length
         self.move_robot(moving_direction, speed, rotation_speed, thrower_speed)
+
 
 if __name__ == "__main__":
     robot = RobotMovement()
     for i in range(25000):
-        robot.move_robot_XY(10, 10, 0)
+        robot.move_robot_XY(20, 0, 0)
