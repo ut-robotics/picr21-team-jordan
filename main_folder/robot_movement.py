1b57ac39c533dd81c75239e4c1a0b3f9dfdc1e11
Leonid-98 <root@root.com>
2021-11-11 17:37:57 +0200

    fixed omni motional mooving

--- main_folder/robot_movement.py
+++ main_folder/robot_movement.py
@@ -12,7 +12,7 @@ class SerialPortNotFound(Exception):
 
 
 STM_32_HWID = "USB VID:PID=0483:5740"
-WHEEL_ANGLES = [120, 0, -120]
+WHEEL_ANGLES = [+120, 0, -120]
 
 
 class RobotMovement:
@@ -28,25 +28,40 @@ class RobotMovement:
             if STM_32_HWID in hwid:
                 serial_port = devices[hwid]
                 break
-        
+
         if serial_port is None:
-            raise SerialPortNotFound 
+            raise SerialPortNotFound
         self.ser = serial.Serial(serial_port, 115200)
 
-    def calculate_speed(self, degrees, moving_direction, speed_limit, state):
-        return int(math.sin((moving_direction + degrees) * (2 * math.pi / 360)) * (speed_limit)) if state == "transition" else speed_limit
+    def calculate_speed(self, wheel_angle_degrees, moving_direction, speed, rotation_speed):
+        return int(math.sin((moving_direction + wheel_angle_degrees) * (2 * math.pi / 360)) * speed + rotation_speed)
 
-    def move_robot(self, state="transition", moving_direction=0, speed_limit=10, thrower_speed=0, failsafe=0):
-        if not self.ser.isOpen():
-            self.ser.open()
+    def move_robot(self, moving_direction=0, speed=0, rotation_speed=0, thrower_speed=0, failsafe=0):
+        speed1, speed2, speed3 = [self.calculate_speed(angle, moving_direction, speed, rotation_speed) for angle in WHEEL_ANGLES]
 
-        speed1, speed2, speed3 = [self.calculate_speed(angle, moving_direction, speed_limit, state) for angle in WHEEL_ANGLES]
+        send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
+        self.ser.write(send_data)
 
-        try:
-            send_data = struct.pack("<hhhHBH", speed1, speed2, speed3, thrower_speed, failsafe, 0xAAAA)
-            self.ser.write(send_data)
-            received_data = self.ser.read(8)
-            actual_speed1, actual_speed2, actual_speed3, feedback_delimiter = struct.unpack("<hhhH", received_data)
-            # print(f"{speed1}/{actual_speed1} | {speed2}/{actual_speed2} | {speed3}{actual_speed3}")
-        except KeyboardInterrupt:
-            pass
+    def move_robot_XY(self, speed_x=0, speed_y=0, rotation_speed=0, thrower_speed=0):
+        def degrees(rad):
+            return(rad * 180 / math.pi )
+
+        pure_deg = degrees(-math.atan2(speed_y, speed_x))
+        moving_direction = 0
+        if speed_x > 0 and speed_y > 0:
+            moving_direction = 90 + pure_deg 
+        if speed_x > 0 and speed_y < 0: 
+            moving_direction = 90 - pure_deg
+        if speed_x < 0 and speed_y < 0:
+            moving_direction = 270 + pure_deg
+        if speed_x < 0 and speed_y > 0:
+            moving_direction = 180 + pure_deg
+            
+        speed = math.hypot(speed_x, speed_y)  # vector length
+        print(speed)
+        self.move_robot(moving_direction, speed, rotation_speed, thrower_speed)
+
+if __name__ == "__main__":
+    robot = RobotMovement()
+    for i in range(50000):
+        robot.move_robot_XY(0, 0, -10)
