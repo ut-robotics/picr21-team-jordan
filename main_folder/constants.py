97181dc11163accf35eb06aed7a381dae9a0f029
Leonid-98 <leonid.tsigrinski@gmail.com>
2021-11-17 17:09:40 +0200

    added comments

--- main_folder/constants.py
+++ main_folder/constants.py
@@ -1,3 +1,4 @@
+# frame size constants
 WIDTH = 960
 HEIGHT = 540
 WIDTH_DEPTH = 848
@@ -5,6 +6,7 @@ HEIGHT_DEPTH = 480
 CROP_Y1 = 170
 CROP_Y2 = 350
 
+# constants for names of windows
 BASKET = "basket"
 BALL = "ball"
 ORIGINAL_WINDOW = "Original"
@@ -13,9 +15,11 @@ MASKED_WINDOW_BASKET = "Basket"
 DEPTH_WINDOW = "Depth"
 TRACKBAR_WINDOW = "Trackbar"
 
-MINIMAL_BALL_SIZE_TO_DETECT = 7 #TODO delete mb
+# ball detection constants
+MINIMAL_BALL_SIZE_TO_DETECT = 7  # TODO delete mb
 MIN_BALL_RADIUS = 20
 
+# center coordinates and range (that robot claims as center) constants
 CENTER_OFFSET_X = 30
 CENTER_OFFSET_Y = 20
 CENTER_X = int(WIDTH / 2)
@@ -23,6 +27,6 @@ CENTER_Y = int(CROP_Y2 / 2)
 CENTER_RANGE_X = range(CENTER_X - CENTER_OFFSET_X, CENTER_X + CENTER_OFFSET_X, 1)
 CENTER_RANGE_Y = range(CENTER_Y - CENTER_OFFSET_Y, CENTER_Y + CENTER_OFFSET_Y, 1)
 
+# speed calculation constants
 ROT_MULTIPLIER = 10
 Y_MULTIPLIER = 5
-
