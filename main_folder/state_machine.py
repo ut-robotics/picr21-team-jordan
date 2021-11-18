16254aca88d8b198f927b40cf540dbbbeddc1bd2
our robot <blabla@blala.com>
2021-11-18 16:48:30 +0200

    small things impelemted, needed to be raftored

--- main_folder/state_machine.py
+++ main_folder/state_machine.py
@@ -41,12 +41,13 @@ class StateMachine:
         if ball_x == -1:
             self.Robot.move_robot_XY(0, 0, 15)
 
-        elif ball_x in const.CENTER_RANGE_X:
-            self.Robot.move_robot_XY(0, 0, 0)
-            self.state = State.GET_TO_BALL
+        # elif ball_x in const.CENTER_RANGE_X:
+        #     self.Robot.move_robot_XY(0, 0, 0)
+        #     self.state = State.GET_TO_BALL
 
         else:
-            self.Robot.move_robot_XY(0, 0, robot_speed_rot)
+            # self.Robot.move_robot_XY(0, 0, robot_speed_rot)
+            self.state = State.GET_TO_BALL
 
     def get_to_ball(self, ball_x, ball_y, ball_radius):
         """State.GET_TO_BALL action"""
@@ -55,7 +56,7 @@ class StateMachine:
 
         if ball_y == -1:
             self.state = State.FIND_BALL
-        elif ball_y in const.CENTER_RANGE_Y and ball_radius > const.MIN_BALL_RADIUS:
+        elif ball_y in const.CENTER_RANGE_Y and ball_radius > const.MIN_BALL_RADIUS_TO_GET:
             self.Robot.move_robot_XY(0, 0, 0)
         else:
             self.Robot.move_robot_XY(0, robot_speed_y, robot_speed_rot)
@@ -63,11 +64,15 @@ class StateMachine:
     def calculate_rotation_speed(self, ball_x):
         """Rot speed is linear dependence"""
         speed_rot = (const.CENTER_X - ball_x) / const.ROT_MULTIPLIER
-        return int(speed_rot)
+        if speed_rot > const.MAXIMUM_SPEED:
+            speed_rot = const.MAXIMUM_SPEED
+        return int(speed_rot) if speed_rot <= const.MAXIMUM_SPEED else const.MAXIMUM_SPEED
 
     def calculate_y_speed(self, ball_y):
         """Y speed is cubic dependence"""
-        speed_y = ((const.CENTER_Y - ball_y) / const.Y_MULTIPLIER) ** 3
+        speed_y = int(((const.CENTER_Y - ball_y) / const.Y_MULTIPLIER) ** 3)
+        if speed_y > const.MAXIMUM_SPEED:
+            speed_y = const.MAXIMUM_SPEED
         return int(speed_y)
 
     def calculate_x_speed(self, tba):
