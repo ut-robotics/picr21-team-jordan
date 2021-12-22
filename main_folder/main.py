e4172285d9b0df445a58e9e8a789d7cb9ab35f12
Leonid-98 <leonid.tsigrinski@gmail.com>
2021-12-23 00:28:35 +0200

    github issue fix

--- main_folder/main.py
+++ main_folder/main.py
@@ -11,6 +11,7 @@ from image_processor import ImageProcessor
 from robot_gui import RobotGui
 from socket_data_getter import SocketDataGetter
 from state_machine import StateMachine
+from manual_control import ManualController
 
 
 class Main:
@@ -20,80 +21,87 @@ class Main:
     """
 
     def __init__(self, enable_gui):
-        self.enable_gui = enable_gui
-        self.gui = RobotGui() if enable_gui else None
-        self.state_machine = StateMachine()
         self.cam = RealsenseCamera()
         self.cam.open()
+        self.gui = RobotGui() if enable_gui else None
+        self.state_machine = StateMachine()
         self.image_processor = ImageProcessor(self.cam)
+        self.manual_controller = ManualController()
 
         self.target_basket = GameObject.BASKET_BLUE
         self.robot_id = "001TRT"
         self.current_state = State.INITIAL
         self.run = False
 
+        self.enable_gui = enable_gui
         self.fps = 0
 
     def main(self, socket_data):
-        q = [0, 0, 0]
+        manual_controller = self.manual_controller
+        manual_controller.main()
         while True:
             start_time = time.time()
 
-            # check for referee commands
-            if socket_data:
-                command = socket_data.pop(0)
-                try:
-                    if command["signal"] == "start":
-                        index = command["targets"].index(self.robot_id)
-                        target_basket = command["baskets"][index]
-                        self.target_basket = GameObject.BASKET_BLUE if target_basket == "blue" else GameObject.BASKET_ROSE
-                        self.run = True
+            # check if manual control is enabled
+            if manual_controller.is_enabled:
+                manual_controller.robot.move_robot_XY(manual_controller.speed_x, manual_controller.speed_y, manual_controller.speed_rot)
+            else:
+                # check for referee commands
+                if socket_data:
+                    command = socket_data.pop(0)
+                    try:
+                        if command["signal"] == "start":
+                            index = command["targets"].index(self.robot_id)
+                            target_basket = command["baskets"][index]
+                            self.target_basket = GameObject.BASKET_BLUE if target_basket == "blue" else GameObject.BASKET_ROSE
+                            self.run = True
 
-                    elif command["signal"] == "stop":
-                        index = command["targets"].index(self.robot_id)
-                        self.run = False
-                except ValueError:
-                    # target isn't our robot id
-                    pass
+                        elif command["signal"] == "stop":
+                            index = command["targets"].index(self.robot_id)
+                            self.run = False
+                    except ValueError:
+                        # target isn't our robot id
+                        pass
 
-            print(self.run, self.target_basket)
+                print(self.run, self.target_basket)
 
-            # detect all objects
-            # aligned_depth = True if self.current_state == State.THROW else False
-            aligned_depth = True
-            results = self.image_processor.process_frame(aligned_depth=aligned_depth)
-            ball_x, ball_y, ball_radius = -1, -1, -1
-            basket_x, basket_y, basket_radius = -1, -1, -1
-            basket_dist = -1
+                # detect all objects
+                # aligned_depth = True if self.current_state == State.THROW else False
+                aligned_depth = True
+                results = self.image_processor.process_frame(aligned_depth=aligned_depth)
+                ball_x, ball_y, ball_radius = -1, -1, -1
+                basket_x, basket_y, basket_radius = -1, -1, -1
+                basket_dist = -1
 
-            if results.balls:
-                ball = results.balls[0]
-                ball_x = ball.x
-                ball_y = ball.y
-                ball_radius = int(ball.width / 2)
+                if results.balls:
+                    ball = results.balls[0]
+                    ball_x = ball.x
+                    ball_y = ball.y
+                    ball_radius = int(ball.width / 2)
 
-                basket = results.basket_b if self.target_basket == GameObject.BASKET_BLUE else results.basket_m
-                if basket.exists:
-                    basket_x = basket.x
-                    basket_y = basket.y
-                    basket_radius = int(basket.width / 2)
-                    basket_dist = int(round(basket.distance * 100))
+                    basket = results.basket_b if self.target_basket == GameObject.BASKET_BLUE else results.basket_m
+                    if basket.exists:
+                        basket_x = basket.x
+                        basket_y = basket.y
+                        basket_radius = int(basket.width / 2)
+                        basket_dist = int(round(basket.distance * 100))
 
-            # run robot
-            if self.run:
-                self.current_state = self.state_machine.run_current_state(ball_x, ball_y, basket_x, basket_dist)
+                # run robot
+                if self.run:
+                    self.current_state = self.state_machine.run_current_state(ball_x, ball_y, basket_x, basket_dist)
 
-            # show gui
-            if self.enable_gui:
-                ball_info = [ball_x, ball_y, ball_radius, (ball_x, ball_y)]
-                basket_info = [basket_x, basket_y, basket_radius, (basket_x, basket_y)]
+                # show gui
+                if self.enable_gui:
+                    ball_info = [ball_x, ball_y, ball_radius, (ball_x, ball_y)]
+                    basket_info = [basket_x, basket_y, basket_radius, (basket_x, basket_y)]
 
-                self.gui.update_image(results.color_frame)
-                self.gui.update_info(self.fps, self.current_state, ball_info, basket_info)
-                self.gui.show_gui()
+                    self.gui.update_image(results.color_frame)
+                    self.gui.update_info(self.fps, self.current_state, ball_info, basket_info)
+                    self.gui.show_gui()
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
+
             self.fps = round(1.0 / (time.time() - start_time), 2)
 
         self.cam.close()
--- main_folder/manual_control.py
+++ main_folder/manual_control.py
@@ -9,22 +9,20 @@ class ManualController:
         self.speed_x = 0
         self.speed_y = 0
         self.speed_rot = 0
+        self.enable = False
         self.kill = False
 
-    def main(self):
-        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
-        listener.start()
-        print("Robot is running. Press ESC to finish.")
+    def is_enabled(self):
+        return self.enable
 
-        while True:
-            if self.kill:
-                break
-            self.robot.move_robot_XY(self.speed_x, self.speed_y, self.speed_rot)
+    def main(self):
+        listener = keyboard.Listener(on_press=self.on_press, on_release=self.nothing)
+        listener.start()
 
     def on_press(self, key):
         try:
             if key.char == "w":
-                self.speed_y = self.max_speed if self.speed_y == 0 else 0          
+                self.speed_y = self.max_speed if self.speed_y == 0 else 0
             elif key.char == "s":
                 self.speed_y = -self.max_speed if self.speed_y == 0 else 0
             elif key.char == "d":
@@ -33,21 +31,20 @@ class ManualController:
                 self.speed_x = -self.max_speed if self.speed_x == 0 else 0
             elif key.char == "e":
                 self.speed_rot = int(-self.max_speed / 3) if self.speed_rot == 0 else 0
-            elif key.char == "q":
+            elif key.char == "r":
                 self.speed_rot = int(self.max_speed / 3) if self.speed_rot == 0 else 0
-            elif key.char == "t":
+            elif key.char == "e":
                 self.speed_rot = 0
                 self.speed_x = 0
                 self.speed_y = 0
+            elif key.char == "g":
+                self.enable = True if not self.enable else False
         # special key pressed
         except AttributeError:
             pass
 
-    def on_release(self, key):
-        if key == keyboard.Key.esc:
-            print("Finished.")
-            self.kill = True
-            return False
+    def nothing(self):
+        pass
 
 
 if __name__ == "__main__":
